b60a15367cef0c326ff409c6cdeba7561293ecd7
Brynn Stafford <54415447+brynnstafford@users.noreply.github.com>
2020-02-10 14:57:35 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,8 @@ Brynn Stafford
 using google java style guide for standard formatting
 
 
-(1)1st Assignment - Say No to Hello World
+##### 1st Assignment - Say No to Hello World
+1st Assignment - Say No to Hello World
 This isn't our first rodeo, so I'm not going to insult anyone with hello world:
 
 Instead print out the following pattern:
@@ -13,19 +14,18 @@ Instead print out the following pattern:
 ##
 #
 
-
 class Main {
   public static void main(String[] args) {
     //Printing out a pattern
     System.out.print("*\n**\n***\n");
     System.out.println("##\n#");
-    
-    
   }
 }
 
 
-(2)Variable Assignment:
+
+##### Assignment 2 - Variable Assignment
+Variable Assignment:
 When we create a variable we also assign it (no matter what)
 The above (or something near it) was stated in class, consider what happens when we just "create" a variable without assigning it? (hint: print it). Then complete the following assignment:
 
@@ -34,18 +34,18 @@ Create an integer variable on line 4 but do not assign it (what is its value?)
 Now assign the integer the value 21 on line 5.
 Finally, change the initial creation of the variable (line 4) to assign the value 21 and remove line 5.
   
-
 class Main {
   public static void main(String[] args) {
     // Assigning variables
     int num1 = 21;
-    
     System.out.print(num1);
   }
 }
 
 
-(3)Assign and Create 2
+
+##### Assignment 3 - Creating and printing Vars 2
+Assign and Create 2
 
 For you to do:
 Create a String variable called "name" and set it to "Chen"
@@ -61,13 +61,14 @@ class Main {
     String name = "Chen";
     int age = 50;
     int iq = age;
-    
     System.out.println(name + "\n" + age + iq);
   }
 }
 
 
-(4)The variable "num" holds an integer user input
+
+##### Assignment 4: If practice
+The variable "num" holds an integer user input
 Write a conditional statement starting on line 9 that does the following:
 If num is positive, print "__ is positive"
 If num is negative, print "__ is negative"
@@ -78,10 +79,8 @@ In: -2
 -2 is negative
 In: 0
 (no output for zero) 
-
   
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
   	Scanner inp = new Scanner(System.in);
@@ -99,8 +98,10 @@ class Main {
 }
 
 
-(5)MUST USE SWITCH STATEMENT
-  The variable "name" holds a String user input 
+
+##### Assignment 5: Switch Practice
+MUST USE SWITCH STATEMENT
+The variable "name" holds a String user input 
 
 Write a conditional statement starting on line 9 that does the following:
 (Case counts for this one - so "Chen" is different than "chen")
@@ -118,19 +119,15 @@ TA
 In: chen
 student
 
-
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
   	Scanner scan = new Scanner(System.in);
   	System.out.print("In:");
   	String name = scan.nextLine();
-  	
   	/*Using switches with strings.
   	* Printing specific output based on string input
   	*/
-  	
   	String x = name;
   	switch (x) {
   	  case "Chen": case "Greenwell": case "Bob":
@@ -142,37 +139,36 @@ class Main {
   	  default:
   	   System.out.println("student");
   	}
-  	
   }
 }
 
 
-(6)Given a String (already declared for you as str), do the following:
+
+##### Assignment 6: String Methods 1
+Given a String (already declared for you as str), do the following:
 Print out the following: "The first 3 letters of ___ is ___"
 
 For example, if the input is "banana", your output should be "The first 3 letters of banana is ban"
 verify = type is correct & exists
 validate = data is what I expect
 
-
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
   	Scanner inp = new Scanner(System.in);
   	System.out.print("In:");
   	String str = inp.nextLine();
-  	
   	// Checks input and gives first 3 letters (or >= 3)
   	if(str.length() > 2) {
   	  System.out.println("The first 3 letters of " + str + " is " + str.substring(0,3));
   	}
-
   }
 }
 
 
-(7)Given three variables:
+
+##### Assignment 7: String Methods 2
+Given three variables:
 String str
 int start
 int end
@@ -193,7 +189,6 @@ lolwut
 012345
 
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
   	Scanner inp = new Scanner(System.in);
@@ -210,7 +205,9 @@ class Main {
 }
 
 
-(8)On line 5, declare an integer array of size 5 and assign the values 10, 20, 30, 40, and 50 to it.
+
+##### Assignment 8 - Looping Arrays
+On line 5, declare an integer array of size 5 and assign the values 10, 20, 30, 40, and 50 to it.
 
 Then use a for loop to print out all the elements of the array, one per line.
   
@@ -233,7 +230,10 @@ class Main
 	}
 }
 
-(9)Inputs:
+
+
+##### Assignment 9: For loop 1
+Inputs:
 int end;
 
 Write a for loop that will print out the numbers starting at 1 and ending at the input inclusive.  The numbers printed should all be on the same line separated by a space.
@@ -250,7 +250,6 @@ In: -5
 (no output)
   
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
     Scanner inp = new Scanner(System.in);
@@ -262,6 +261,8 @@ class Main {
     }
   }
 }
+
+
 
 ##### Assignment 10: Fibonacci Loop
 
@@ -275,9 +276,7 @@ Write some code to print out the first X numbers of the fibonacci sequence.
 
 Your output should be on one line, with each number separated by a space.  You may assume that x is at least 2.
 
-
 import java.util.Scanner;
-
 class Main {
   public static void main(String[] args) {
     Scanner inp = new Scanner(System.in);
@@ -285,7 +284,6 @@ class Main {
     int x = inp.nextInt();
    	// Assigning first two fibonacci int values (f1 & f2)
    	int f1 = 0, f2 = 1;
-   	
    	// While loop condition states loop must start at f1 
    	// & input must be greater than f1
    	int i = 1;
@@ -299,6 +297,13 @@ class Main {
    	}
   }
 }
+
+
+
+
+
+
+
 
 
 
